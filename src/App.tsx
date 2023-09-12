b1fa446cfd7850487d29a3c212c0ea430b2984e4
carolcarvalhosa <ccscarvalhosa@gmail.com>
2023-09-12 18:57:02 -0300

    hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import Routes from './routes';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 function App() {
   return (
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <HashRouter>
       <Routes />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
